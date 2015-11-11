ce135c9ef80ffe981b79c0c1c6c0b8ab38236047
vrezepova <rezepova@gmail.com>
2015-11-10 18:37:26 -0800

    Added  some practice examples

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,9 +1,14 @@
 require 'rubygems'
 require 'selenium-webdriver'
 require 'cucumber'
+require 'spreadsheet'
 
 # $driver = Selenium::WebDriver.for :firefox
 # $driver = Selenium::WebDriver.for :chrome
 $driver = Selenium::WebDriver.for :safari
 $dropdown = Selenium::WebDriver:: Support:: Select
 
+def click_button_w_text(text)
+ btn = $driver.find_element(:xpath, "//button[text() = '"+text+"']")
+ btn.click
+end
